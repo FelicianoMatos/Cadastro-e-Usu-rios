12977f44ae465d68c1632234988d082eb9c043b6
Feliciano Matos <feli_matos@hotmail.com>
2022-10-26 22:22:31 -0300

    commit

---
+++ src/components/home/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/main"
+
+export default props =>
+<Main icon="home" title="início"
+       subtitle="Projeto Cadastro de Usuários">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className='mb-0'> Ao Sistema de Cadastro</p>
+    </Main>
---
+++ src/components/template/footer.jsx
@@ -0,0 +1,10 @@
+import './footer.css'
+import React from 'react'
+
+export default props =>
+ <footer className="footer">
+    <span>
+      desenvolvido com <i className='fa fa-heart text-danger'></i> por
+      <strong> Programador<span className='text-danger'>3</span></strong>
+    </span>
+ </footer>
